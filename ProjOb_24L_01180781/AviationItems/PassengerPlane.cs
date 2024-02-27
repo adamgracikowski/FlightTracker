ae45ce4b5ed0fc14bd3b8f10fa5ed835a6c4e0b7
Adam <admgrac@gmail.com>
2024-02-27 14:51:22 +0100

    add PassengerPlane class

---
+++ ProjOb_24L_01180781/AviationItems/PassengerPlane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public class PassengerPlane
+        : Plane, IAviationItem
+    {
+        public static string Acronym => _acronym;
+        public ClassSize ClassSize { get; private set; }
+        public PassengerPlane(UInt64 id, string serial, string country, string model, ClassSize classSize)
+            : base(id, serial, country, model)
+        {
+            ClassSize = classSize;
+        }
+
+        private static readonly string _acronym = "PP";
+    }
+}
