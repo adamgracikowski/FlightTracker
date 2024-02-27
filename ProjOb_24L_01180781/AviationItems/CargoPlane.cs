4756ee810abc86de16daa0a1cc0cc61166ccbe45
Adam <admgrac@gmail.com>
2024-02-27 14:52:07 +0100

    add CargoPlane class

---
+++ ProjOb_24L_01180781/AviationItems/CargoPlane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public class CargoPlane
+        : Plane, IAviationItem
+    {
+        public static string Acronym => _acronym;
+        public Single MaxLoad { get; private set; }
+
+        public CargoPlane(UInt64 id, string serial, string country, string model, Single maxLoad)
+            : base(id, serial, country, model)
+        {
+            MaxLoad = maxLoad;
+        }
+
+        private static readonly string _acronym = "CP";
+    }
+}
