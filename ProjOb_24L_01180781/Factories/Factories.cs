ce54285c075d7c5a56077cab08cda5b760516790
Adam <admgrac@gmail.com>
2024-02-27 14:58:00 +0100

    add CrewFactory class

--- ProjOb_24L_01180781/Factories/Factories.cs
+++ ProjOb_24L_01180781/Factories/Factories.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjOb_24L_01180781.AviationItems;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace ProjOb_24L_01180781.Factories
 {
-    internal class Factories
+    public class CrewFactory
+        : IAviationFactory
     {
+        public IAviationItem Create(string[] itemDetails)
+        {
+            return new Crew(
+                id: UInt64.Parse(itemDetails[1]),
+                name: itemDetails[2],
+                age: UInt64.Parse(itemDetails[3]),
+                phone: itemDetails[4],
+                email: itemDetails[5],
+                practice: UInt16.Parse(itemDetails[6]),
+                role: itemDetails[7]
+            );
+        }
     }
 }
