fbe74627aeb32929e782b72538174b931df0aae8
Adam <admgrac@gmail.com>
2024-02-27 15:04:47 +0100

    add FtrFormatException class

--- ProjOb_24L_01180781/Exceptions/AviationExceptions.cs
+++ ProjOb_24L_01180781/Exceptions/AviationExceptions.cs
@@ -19,4 +19,32 @@ namespace ProjOb_24L_01180781.Exceptions
         public AviationException(string? message, Exception? innerException)
             : base(message, innerException) { }
     }
+    /// <summary>
+    /// Represents all the exceptions connected with .ftr file formatting issues.
+    /// </summary>
+    public class FtrFormatException
+        : AviationException
+    {
+        public FtrFileContext? Context { get; private set; }
+        public FtrFormatException()
+            : base() { }
+        public FtrFormatException(string? message, FtrFileContext? context = null)
+            : base(CreateMessage(message, context))
+        {
+            Context = context;
+        }
+        public FtrFormatException(string? message, Exception? innerException, FtrFileContext? context = null)
+            : base(CreateMessage(message, context), innerException)
+        {
+            Context = context;
+        }
+        private static string? CreateMessage(string? message, FtrFileContext? context = null)
+        {
+            if (message is not null && context is not null)
+            {
+                message += $" in file {context.Filename} (in line {context.LineNumber}";
+            }
+            return message;
+        }
+    }
 }
