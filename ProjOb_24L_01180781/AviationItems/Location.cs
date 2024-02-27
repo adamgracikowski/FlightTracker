8f0ce7eb8394b967ca683ee5870dab88b3d7deaa
Adam <admgrac@gmail.com>
2024-02-27 14:52:49 +0100

    add Location struct

---
+++ ProjOb_24L_01180781/AviationItems/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public struct Location
+    {
+        public Single Longitude { get; set; }
+        public Single Latitude { get; set; }
+        public Single Amsl { get; set; }
+        public Location(Single longitude, Single latitude, Single amsl)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+            Amsl = amsl;
+        }
+    }
+}
