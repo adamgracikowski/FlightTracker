aee5a94ca904262b2417327979c2bc1a87aa53a8
Adam <admgrac@gmail.com>
2024-02-27 15:03:36 +0100

    add FtrFileContext class

---
+++ ProjOb_24L_01180781/Exceptions/FtrFileContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Exceptions
+{
+    public class FtrFileContext
+    {
+        public string Filename { get; set; }
+        public UInt64 LineNumber { get; set; }
+
+        public FtrFileContext(string filename, ulong lineNumber)
+        {
+            Filename = filename;
+            LineNumber = lineNumber;
+        }
+    }
+}
