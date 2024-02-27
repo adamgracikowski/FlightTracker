64dc7b2d6f8e653a8db0bb54d45ac32f49502358
Adam <admgrac@gmail.com>
2024-02-27 15:02:39 +0100

    add ParseExtender class

---
+++ ProjOb_24L_01180781/Tools/ParseExtender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Tools
+{
+    /// <summary>
+    /// Contains helper methods for parsing data out of a string object.
+    /// </summary>
+    public static class ParseExtender
+    {
+        /// <summary>
+        /// This method parses an array of type T out of a string object.
+        /// Elements of the parsed array are separated by one of the separator elements.
+        /// Empty entries are removed.
+        /// </summary>
+        public static T[] ParseToArraySeparated<T>(this string data, char[] separator)
+            where T : IParsable<T>
+        {
+            return data.Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                       .Select(x => T.Parse(x, CultureInfo.InvariantCulture))
+                       .ToArray();
+        }
+    }
+}
