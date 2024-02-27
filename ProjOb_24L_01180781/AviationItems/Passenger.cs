ce65f262c7e39dcd26b6a5407890717973725964
Adam <admgrac@gmail.com>
2024-02-27 14:49:47 +0100

    add Passenger class

---
+++ ProjOb_24L_01180781/AviationItems/Passenger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public class Passenger
+        : Person, IAviationItem
+    {
+        public static string Acronym => _acronym;
+        public string Class { get; private set; }
+        public UInt64 Miles { get; private set; }
+
+        public Passenger(UInt64 id, string name, UInt64 age, string phone, string email, string planeClass, UInt64 miles)
+            : base(id, name, age, phone, email)
+        {
+            Class = planeClass;
+            Miles = miles;
+        }
+
+        private static readonly string _acronym = "P";
+    }
+}
