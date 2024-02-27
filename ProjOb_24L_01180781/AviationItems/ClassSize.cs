27a3e5fad3f4b4fa2088b2f011da3eb1a333acc7
Adam <admgrac@gmail.com>
2024-02-27 14:53:45 +0100

    add ClassSize struct

---
+++ ProjOb_24L_01180781/AviationItems/ClassSize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public struct ClassSize
+    {
+        public UInt16 First { get; set; }
+        public UInt16 Business { get; set; }
+        public UInt16 Economy { get; set; }
+        public ClassSize(UInt16 first, UInt16 business, UInt16 economy)
+        {
+            First = first;
+            Business = business;
+            Economy = economy;
+        }
+    }
+}
