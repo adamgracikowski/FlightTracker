595705a40904f78d0df056d91dc23684b4cb73c9
Adam <admgrac@gmail.com>
2024-02-27 14:54:48 +0100

    add Airplane class

---
+++ ProjOb_24L_01180781/AviationItems/Airport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public class Airport
+        : IAviationItem
+    {
+        public static string Acronym => _acronym;
+        public UInt64 Id { get; private set; }
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+        public Location Location { get; private set; }
+        public string Country { get; private set; }
+
+        public Airport(UInt64 id, string name, string code, Location location, string country)
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            Location = location;
+            Country = country;
+        }
+
+        private static readonly string _acronym = "AI";
+    }
+}
