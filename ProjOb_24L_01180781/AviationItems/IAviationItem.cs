0e2177d6ec417b33f3da2d3230a7ee53fe035a1b
Adam <admgrac@gmail.com>
2024-02-27 14:48:19 +0100

    add IAviationItem interface

---
+++ ProjOb_24L_01180781/AviationItems/IAviationItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    /// <summary>
+    /// Represents common characteristics of all classes,
+    /// which instances are stored in .ftr files.
+    /// </summary>
+    public interface IAviationItem
+    {
+        /// <summary>
+        /// Uniquely identifies a class, for which details of the instance of the class, 
+        /// are stored in every line of .ftr file.
+        /// </summary>
+        public static string Acronym { get; } = string.Empty;
+    }
+}
---
+++ ProjOb_24L_01180781/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjOb_24L_01180781
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
