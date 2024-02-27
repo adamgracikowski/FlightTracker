46fc181e94fb9cbf51a994ec7f8c2fe6daee567e
Adam <admgrac@gmail.com>
2024-02-27 14:50:21 +0100

    add Crew class

---
+++ ProjOb_24L_01180781/AviationItems/Crew.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public class Crew
+        : Person, IAviationItem
+    {
+        public static string Acronym => _acronym;
+        public UInt16 Practice { get; private set; }
+        public string Role { get; private set; }
+
+        public Crew(UInt64 id, string name, UInt64 age, string phone, string email, UInt16 practice, string role)
+            : base(id, name, age, phone, email)
+        {
+            Practice = practice;
+            Role = role;
+        }
+
+        private static readonly string _acronym = "C";
+    }
+}
