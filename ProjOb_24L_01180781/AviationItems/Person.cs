374d8cbce6169683eb4fe6767dbce186e19ef320
Adam <admgrac@gmail.com>
2024-02-27 14:49:12 +0100

    add Person class

---
+++ ProjOb_24L_01180781/AviationItems/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.AviationItems
+{
+    public abstract class Person
+    {
+        public UInt64 Id { get; private set; }
+        public string Name { get; private set; }
+        public UInt64 Age { get; private set; }
+        public string Phone { get; private set; }
+        public string Email { get; private set; }
+
+        public Person(UInt64 id, string name, UInt64 age, string phone, string email)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
