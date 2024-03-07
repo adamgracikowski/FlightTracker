ff171f56d2e8aca735f26352e7ffd08e1e3164b9
Adam <admgrac@gmail.com>
2024-03-07 13:34:57 +0100

    add NetworkSourceSimulator.dll reference

--- ProjOb_24L_01180781/DataManagers/TcpDataManager.cs
+++ ProjOb_24L_01180781/DataManagers/TcpDataManager.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetworkSourceSimulator;
+
+using Nss = NetworkSourceSimulator;
+
 
 namespace ProjOb_24L_01180781.DataManagers
 {
