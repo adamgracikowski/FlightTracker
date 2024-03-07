d5c7ee164d36f9c3da9332bf78c0abc0a28c663b
Adam <admgrac@gmail.com>
2024-03-07 13:20:30 +0100

    add TcpFormatException class

---
+++ ProjOb_24L_01180781/Exceptions/TcpFormatException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Exceptions
+{
+    public class TcpFormatException : AviationException
+    {
+        public TcpFormatException()
+            : base() { }
+        public TcpFormatException(string? message)
+            : base(message) { }
+        public TcpFormatException(string? message, Exception? innerException)
+            : base(message, innerException) { }
+    }
+}
