3c1a4b3a52825d7f81e6453c7875c99f08e5da85
Adam <admgrac@gmail.com>
2024-03-07 13:17:24 +0100

    add MsConverter class

---
+++ ProjOb_24L_01180781/Tools/MsConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Tools
+{
+    public static class MsConverter
+    {
+        public static string SinceEpochUtc(Int64 ms)
+        {
+            var dateTime = DateTimeOffset.FromUnixTimeMilliseconds(ms).UtcDateTime;
+            var formattedTime = dateTime.ToString("HH:mm");
+            return formattedTime;
+        }
+    }
+}
