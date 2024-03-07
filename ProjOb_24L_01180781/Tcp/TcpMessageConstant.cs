89350444d4e018312acc70cfb50b1d89c6804db2
Adam <admgrac@gmail.com>
2024-03-07 13:16:27 +0100

    add TcpMessageConstant class

---
+++ ProjOb_24L_01180781/Tcp/TcpMessageConstant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Tcp
+{
+    public static class TcpMessageConstant
+    {
+        public static readonly int ExtendedAcronymLength = 3;
+        public static readonly int PersonPhoneNumberLength = 12;
+        public static readonly int CargoCodeLength = 6;
+        public static readonly int PlaneSerialLength = 10;
+        public static readonly int IsoCountryCodeLength = 3;
+        public static readonly int AirportCodeLenght = 3;
+    }
+}
