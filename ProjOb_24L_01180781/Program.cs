65e2a55abd64303f0dbe0908f41e9aeda5a9cdea
Adam <admgrac@gmail.com>
2024-03-07 17:13:28 +0100

    refactor Main

--- ProjOb_24L_01180781/Program.cs
+++ ProjOb_24L_01180781/Program.cs
@@ -1,4 +1,6 @@
-﻿using ProjOb_24L_01180781.Exceptions;
+﻿//#define ETAP1
+#define ETAP2
+
 using System.Globalization;
 
 namespace ProjOb_24L_01180781
@@ -7,38 +9,17 @@ namespace ProjOb_24L_01180781
     {
         static void Main(string[] args)
         {
+            var consoleManager = new ConsoleManager(args);
+
             // changing culture options so that the decimal separator was set to '.'
-            var englishCulture = new CultureInfo("en-US");
-            CultureInfo.DefaultThreadCurrentCulture = englishCulture;
-            CultureInfo.DefaultThreadCurrentUICulture = englishCulture;
+            consoleManager.SetCulture(new CultureInfo("en-US"));
 
-            var inputFile = args.Length > 0 ? args[0] : (GetInputFileFromUser() ?? "example_data.ftr");
-            var outputFile = $"{inputFile}.json";
-            var separator = ',';
-
-            try
-            {
-                var entities = AviationDataManager.ParseFtrFile(inputFile, separator);
-                Console.WriteLine($"file {inputFile} parsed successfully.");
-
-                AviationDataManager.SerializeToJson(entities, inputFile);
-                Console.WriteLine($"collection of objects serialized successfully to {outputFile}.");
-            }
-            catch (AviationException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                var message = $"unknown exception caught: {ex.Message}";
-                Console.WriteLine(message);
-            }
-        }
-        private static string? GetInputFileFromUser()
-        {
-            Console.WriteLine("Please provide the path to the input file: ");
-            var path = Console.ReadLine();
-            return path;
+#if ETAP1
+            consoleManager.RunStage01();
+#endif
+#if ETAP2
+            consoleManager.RunStage02();
+#endif
         }
     }
 }
