8c67def6a22efb6aa051b4d23f89f2a10b41d537
Adam <admgrac@gmail.com>
2024-03-07 13:22:43 +0100

    add SnapshotDetails struct

---
+++ ProjOb_24L_01180781/Tools/SnapshotDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Tools
+{
+    public struct SnapshotDetails
+    {
+        public string Name { get; private set; }
+        public int CollectionCount { get; private set; }
+        public TimeSpan TimeTaken { get; private set; }
+        public SnapshotDetails(string name, int collectionCount, TimeSpan timeTaken)
+        {
+            Name = name;
+            CollectionCount = collectionCount;
+            TimeTaken = timeTaken;
+        }
+    }
+}
