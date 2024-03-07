8f1106dcbee97a5fc07e131fe2d579f8511f6894
Adam <admgrac@gmail.com>
2024-03-07 13:13:55 +0100

    add FtrAcronym and TcpAcronym

---
+++ ProjOb_24L_01180781/Ftr/FtrAcronym.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Ftr
+{
+    public static class FtrAcronym
+    {
+        public static readonly string Crew = "C";
+        public static readonly string Passenger = "P";
+        public static readonly string Cargo = "CA";
+        public static readonly string CargoPlane = "CP";
+        public static readonly string PassengerPlane = "PP";
+        public static readonly string Airport = "AI";
+        public static readonly string Flight = "FL";
+    }
+}
---
+++ ProjOb_24L_01180781/Tcp/TcpAcronym.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Tcp
+{
+    public static class TcpAcronym
+    {
+        public static readonly string Crew = "NCR";
+        public static readonly string Passenger = "NPA";
+        public static readonly string Cargo = "NCA";
+        public static readonly string CargoPlane = "NCP";
+        public static readonly string PassengerPlane = "NPP";
+        public static readonly string Airport = "NAI";
+        public static readonly string Flight = "NFL";
+        public static readonly int Length = 3;
+    }
+}
