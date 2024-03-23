7dfc94602a6f2175f31ed216f8f9c343db75afd2
Adam <admgrac@gmail.com>
2024-03-23 22:49:57 +0100

    add GuiTimer class

---
+++ ProjOb_24L_01180781/GUI/GuiTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.GUI
+{
+    public class GuiTimer
+    {
+        public TimerCallback TimerCallback { get; set; }
+        public TimeSpan DueTime { get; set; }
+        public TimeSpan Period { get; set; }
+        public GuiTimer(TimerCallback timerCallback, TimeSpan dueTime, TimeSpan period)
+        {
+            TimerCallback = timerCallback;
+            DueTime = dueTime;
+            Period = period;
+        }
+        public void Start()
+        {
+            _timer ??= new Timer(TimerCallback, null, DueTime, Period);
+        }
+        public void Stop()
+        {
+            if (_timer is not null)
+            {
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
+        private Timer? _timer;
+    }
+}
