61819922ca8c6fde0f8dd7e66049c736a8c8748c
Adam <admgrac@gmail.com>
2024-03-23 22:55:07 +0100

    add GuiManager class

---
+++ ProjOb_24L_01180781/GUI/GuiManager.cs
@@ -0,0 +1,115 @@
+﻿using FlightTrackerGUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.GUI
+{
+    public class GuiManager
+    {
+        public static GuiManager GetInstance()
+        {
+            _instance ??= new GuiManager();
+            return _instance;
+        }
+        public enum RunnerState
+        {
+            NotUsed = 0,
+            InUse = 1,
+            Used = 2
+        }
+        public bool IsRunnerNotUsed
+        {
+            get
+            {
+                lock (_runnerStateLock)
+                {
+                    return _runnerState == RunnerState.NotUsed;
+                }
+            }
+        }
+        public bool IsRunnerInUse
+        {
+            get
+            {
+                lock (_runnerStateLock)
+                {
+                    return _runnerState == RunnerState.InUse;
+                }
+            }
+        }
+        public bool IsRunnerUsed
+        {
+            get
+            {
+                lock (_runnerStateLock)
+                {
+                    return _runnerState == RunnerState.Used;
+                }
+            }
+        }
+        public void Run()
+        {
+            if (IsRunnerInUse || IsRunnerUsed) return;
+            lock (_runnerStateLock)
+            {
+                _runnerState = RunnerState.InUse;
+                _timer?.Start();
+                Task.Factory.StartNew(Runner.Run).ContinueWith(RunContinuation);
+            }
+        }
+
+        private GuiManager()
+        {
+            _timer = new(TimerCallback, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+        private void RunContinuation(Task task)
+        {
+            lock (_runnerStateLock)
+            {
+                _timer?.Stop();
+                _runnerState = RunnerState.Used;
+            }
+            Console.WriteLine("Checking the radar window for possible failures...");
+            if (task.Exception is not null)
+            {
+                foreach (var innerException in task.Exception.InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No failures detected.");
+            }
+            Console.WriteLine("The radar window has been closed.");
+        }
+        private void TimerCallback(object? state)
+        {
+            AviationDatabase.SyncAviationItems();
+
+            List<FlightGUI> flightGUIs;
+            lock (AviationDatabase.FlightDetailsLock)
+            {
+                AviationDatabase.FlightDetails.ForEach(details => details.UpdateFlightLocation());
+                flightGUIs = AviationDatabase.FlightDetails
+                    .Where(flightDetail =>
+                    {
+                        var now = DateTime.UtcNow;
+                        return flightDetail.Flight.TakeOffDateTime <= now &&
+                               now <= flightDetail.Flight.LandingDateTime;
+                    })
+                    .Select(GuiAdapter.FlightDetailsToFlightGui)
+                    .ToList();
+            }
+            Runner.UpdateGUI(new FlightsGUIData(flightGUIs));
+        }
+
+        private RunnerState _runnerState = RunnerState.NotUsed;
+        private readonly object _runnerStateLock = new();
+        private static GuiManager? _instance;
+        private readonly GuiTimer? _timer;
+    }
+}
