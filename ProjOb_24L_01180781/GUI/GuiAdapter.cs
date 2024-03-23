50ccd1618661ba05a75b224335857b2f2147f9cd
Adam <admgrac@gmail.com>
2024-03-23 22:53:19 +0100

    add GuiAdapter class

---
+++ ProjOb_24L_01180781/GUI/GuiAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.GUI
+{
+    public static class GuiAdapter
+    {
+        public static FlightGUI FlightDetailsToFlightGui(FlightDetails flightDetail)
+        {
+            return new FlightGUI()
+            {
+                ID = flightDetail.Flight.Id,
+                MapCoordRotation = flightDetail.Rotation,
+                WorldPosition = new WorldPosition(
+                    flightDetail.Flight.Location.Latitude,
+                    flightDetail.Flight.Location.Longitude)
+            };
+        }
+    }
+}
