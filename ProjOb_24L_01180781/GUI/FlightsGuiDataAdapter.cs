c60a99cf419e7389d9379d71aaa283e3109d3413
Adam <admgrac@gmail.com>
2024-03-25 17:08:09 +0100

    add FlightsGuiDataAdapter class

---
+++ ProjOb_24L_01180781/GUI/FlightsGuiDataAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.GUI
+{
+    public class FlightsGuiDataAdapter : FlightsGUIData
+    {
+        public FlightsGuiDataAdapter(List<FlightDetails> flights)
+        {
+            _flights = flights;
+        }
+        public override int GetFlightsCount()
+        {
+            return _flights.Count;
+        }
+        public override ulong GetID(int index)
+        {
+            return _flights[index].ID;
+        }
+        public override WorldPosition GetPosition(int index)
+        {
+            return _flights[index].WorldPosition;
+        }
+        public override double GetRotation(int index)
+        {
+            return _flights[index].MapCoordRotation;
+        }
+
+        private List<FlightDetails> _flights;
+    }
+}
