1ca24648ac7f0634c4a19496e38f52db2ee560ab
Adam <admgrac@gmail.com>
2024-03-28 20:05:48 +0100

    add IMedia and IReportable interfaces

---
+++ ProjOb_24L_01180781/Media/IMedia.cs
@@ -0,0 +1,17 @@
+﻿using ProjOb_24L_01180781.AviationItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Media
+{
+    public interface IMedia
+    {
+        string Name { get; }
+        string MakeReport(CargoPlane cargoPlane);
+        string MakeReport(PassengerPlane passengerPlane);
+        string MakeReport(Airport airport);
+    }
+}
---
+++ ProjOb_24L_01180781/Media/IReportable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Media
+{
+    public interface IReportable
+    {
+        string AcceptMediaReport(IMedia media);
+    }
+}
