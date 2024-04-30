fde97566fce5891adf0c661e0c737cd871544ab8
Adam <admgrac@gmail.com>
2024-04-30 11:37:30 +0200

    add KeyComparer and DictionaryExtender

---
+++ ProjOb_24L_01180781/Tools/DictionaryExtender.cs
@@ -0,0 +1,24 @@
+﻿using ProjOb_24L_01180781.AviationItems.Interfaces;
+
+namespace ProjOb_24L_01180781.Tools
+{
+    public static class DictionaryExtender
+    {
+        public static (List<KeyValuePair<UInt64, TValue>> True, List<KeyValuePair<UInt64, TValue>> False)
+            PartitionBy<UInt64, TValue>(this IDictionary<UInt64, TValue> dictionary, Predicate<KeyValuePair<UInt64, TValue>> predicate)
+            where TValue : ILockable
+        {
+            var True = new List<KeyValuePair<UInt64, TValue>>();
+            var False = new List<KeyValuePair<UInt64, TValue>>();
+            foreach (var kvp in dictionary)
+            {
+                lock (kvp.Value.Lock)
+                {
+                    if (predicate(kvp)) True.Add(kvp);
+                    else False.Add(kvp);
+                }
+            }
+            return (True, False);
+        }
+    }
+}
---
+++ ProjOb_24L_01180781/Tools/KeyComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ProjOb_24L_01180781.Tools
+{
+    public class KeyComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string? x, string? y)
+        {
+            return string.Equals(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public int GetHashCode([DisallowNull] string obj)
+        {
+            return obj.ToUpper().GetHashCode();
+        }
+    }
+}
