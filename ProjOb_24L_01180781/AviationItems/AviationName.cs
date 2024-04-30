cf80f2a3e518f3280a8f068b758221f5e134794f
Adam <admgrac@gmail.com>
2024-04-30 11:28:42 +0200

    add AviationName class

---
+++ ProjOb_24L_01180781/AviationItems/AviationName.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjOb_24L_01180781.AviationItems
+{
+    public static class AviationName
+    {
+        public static readonly string Airport = "Airport";
+        public static readonly string Crew = "Crew";
+        public static readonly string Cargo = "Cargo";
+        public static readonly string CargoPlane = "CargoPlane";
+        public static readonly string Passenger = "Passenger";
+        public static readonly string PassengerPlane = "PassengerPlane";
+        public static readonly string Flight = "Flight";
+
+        public static readonly HashSet<string> Names
+            = new([Airport, Crew, Cargo, CargoPlane, Passenger, PassengerPlane, Flight], StringComparer.InvariantCultureIgnoreCase);
+    }
+}
