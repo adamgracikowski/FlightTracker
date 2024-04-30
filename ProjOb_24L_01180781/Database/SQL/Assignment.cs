b630bd485e7c21da38a06e02e1efce7a0e4cf050
Adam <admgrac@gmail.com>
2024-04-30 11:48:08 +0200

    add Assignment and QueryColumns

---
+++ ProjOb_24L_01180781/Database/SQL/Assignment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Database.SQL
+{
+    public class Assignment : IEquatable<Assignment>
+    {
+        public Assignment(string field, string value)
+        {
+            Field = field;
+            Value = value;
+        }
+        public string Field { get; set; }
+        public string Value { get; set; }
+        public override string ToString()
+        {
+            return $"({Field}, {Value})";
+        }
+        public bool Equals(Assignment? other)
+        {
+            if (other is null) return false;
+            return string.Compare(Field, other.Field, StringComparison.InvariantCultureIgnoreCase) == 0 &&
+                Value == other.Value;
+        }
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Assignment);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Field, Value);
+        }
+    }
+
+}
---
+++ ProjOb_24L_01180781/Database/SQL/QueryColumns.cs
@@ -0,0 +1,58 @@
+﻿using ProjOb_24L_01180781.Tools;
+
+namespace ProjOb_24L_01180781.Database.SQL
+{
+    public static class QueryColumns
+    {
+        public static HashSet<string> AllColumns { get; set; } = new(new KeyComparer());
+        public static readonly Dictionary<string, HashSet<string>> Dictionary = new(new KeyComparer())
+            {
+                {
+                    "Airport",
+                    new(["ID", "Name", "Code", "WorldPosition.Long", "WorldPosition.Lat", "AMSL", "CountryCode"],
+                        new KeyComparer())
+                },
+                {
+                    "Flight",
+                    new(["ID", "Origin", "Target", "TakeOffTime", "LandingTime", "WorldPosition.Long", "WorldPosition.Lat", "AMSL", "Plane"],
+                        new KeyComparer())
+                },
+                {
+                    "PassengerPlane",
+                    new(["ID", "Serial", "CountryCode", "Model", "FirstClassSize", "BusinessClassSize", "EconomyClassSize"],
+                        new KeyComparer())
+                },
+                {
+                    "CargoPlane",
+                    new(["ID", "Serial", "CountryCode", "Model", "MaxLoad"],
+                        new KeyComparer())
+                },
+                {
+                    "Cargo",
+                    new(["ID", "Weight", "Code", "Description"],
+                        new KeyComparer())
+                },
+                {
+                    "Passenger",
+                    new(["ID", "Name", "Age", "Phone", "Email", "Class", "Miles"],
+                        StringComparer.InvariantCultureIgnoreCase)
+                },
+                {
+                    "Crew",
+                    new(["ID", "Name", "Age", "Phone", "Email", "Practise", "Role"],
+                        new KeyComparer())
+                },
+            };
+
+        private static void BuildAllColumns()
+        {
+            AllColumns = new(StringComparer.InvariantCultureIgnoreCase);
+            foreach (var hashSet in Dictionary.Values)
+                AllColumns.UnionWith(hashSet);
+        }
+        static QueryColumns()
+        {
+            BuildAllColumns();
+        }
+    }
+}
