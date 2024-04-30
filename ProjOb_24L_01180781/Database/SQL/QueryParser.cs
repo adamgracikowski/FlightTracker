fc28226a7c4a03b4bea933c6a881644fb9e1c8df
Adam <admgrac@gmail.com>
2024-04-30 11:49:20 +0200

    add QueryRegex and QueryParser

---
+++ ProjOb_24L_01180781/Database/SQL/QueryParser.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Database.SQL
+{
+    using ProjOb_24L_01180781.ConsoleManagement.Commands;
+    using ProjOb_24L_01180781.Database.SQL.Queries;
+    using ProjOb_24L_01180781.Database.SQL.WhereClause;
+    using System.Text.RegularExpressions;
+
+    public static class QueryParser
+    {
+        public static DeleteQuery? ParseDeleteQuery(string query)
+        {
+            var match = Regex.Match(query, QueryRegex.DeleteBase, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return null;
+
+            var command = match.Groups[1].Value;
+
+            if (string.Compare(command, Delete.ConsoleText, StringComparison.InvariantCultureIgnoreCase) != 0)
+                throw new InvalidDataException();
+
+            var tableName = match.Groups[2].Value;
+            var hasWhereClause = match.Groups[3].Success;
+            var conditions = match.Groups[4].Value;
+
+            if (!hasWhereClause)
+            {
+                return new DeleteQuery(tableName, [], []);
+            }
+            else if (string.IsNullOrEmpty(conditions))
+                return null;
+
+            if (!ParseWhereConditions(conditions, out var whereConditions, out var conjunctions) ||
+                whereConditions is null || conjunctions is null)
+                return null;
+
+            return new DeleteQuery(tableName, whereConditions, conjunctions);
+        }
+        public static DisplayQuery? ParseDisplayQuery(string query)
+        {
+            var match = Regex.Match(query, QueryRegex.DisplayBase, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return null;
+
+            var command = match.Groups[1].Value;
+
+            if (string.Compare(command, Display.ConsoleText, StringComparison.InvariantCultureIgnoreCase) != 0)
+                throw new InvalidDataException();
+
+            var tableName = match.Groups[4].Value;
+            var columns = match.Groups[2].Value;
+
+            if (!ParseDisplayColumns(columns, out var columnsList) || columnsList is null)
+                return null;
+
+            var hasWhereClause = match.Groups[5].Success;
+            var conditions = match.Groups[6].Value;
+
+            if (!hasWhereClause)
+            {
+                return new DisplayQuery(tableName, columnsList, [], []);
+            }
+            else if (string.IsNullOrEmpty(conditions))
+                return null;
+
+            if (!ParseWhereConditions(conditions, out var whereConditions, out var conjunctions) ||
+                whereConditions is null || conjunctions is null)
+                return null;
+
+            return new DisplayQuery(tableName, columnsList, whereConditions, conjunctions);
+        }
+        public static AddQuery? ParseAddQuery(string query)
+        {
+            var match = Regex.Match(query, QueryRegex.AddBase, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return null;
+
+            var command = match.Groups[1].Value;
+
+            if (string.Compare(command, Add.ConsoleText, StringComparison.InvariantCultureIgnoreCase) != 0)
+                throw new InvalidDataException();
+
+            var tableName = match.Groups[2].Value;
+            var assignments = match.Groups[4].Value;
+
+            if (!ParseAssignments(assignments, out var assignmentsList) || assignmentsList is null)
+                return null;
+
+            return new AddQuery(tableName, assignmentsList);
+        }
+        public static UpdateQuery? ParseUpdateQuery(string query)
+        {
+            var match = Regex.Match(query, QueryRegex.UpdateBase, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return null;
+
+            var command = match.Groups[1].Value;
+
+            if (string.Compare(command, Update.ConsoleText, StringComparison.InvariantCultureIgnoreCase) != 0)
+                throw new InvalidDataException();
+
+            var tableName = match.Groups[2].Value;
+            var assignments = match.Groups[4].Value;
+
+            if (!ParseAssignments(assignments, out var assignmentsList) || assignmentsList is null)
+                return null;
+
+            var hasWhereClause = match.Groups[5].Success;
+            var conditions = match.Groups[6].Value;
+
+            if (!hasWhereClause)
+            {
+                return new UpdateQuery(tableName, assignmentsList, [], []);
+            }
+            else if (string.IsNullOrEmpty(conditions))
+                return null;
+
+            if (!ParseWhereConditions(conditions, out var whereConditions, out var conjunctions) ||
+                whereConditions is null || conjunctions is null)
+                return null;
+
+            return new UpdateQuery(tableName, assignmentsList, whereConditions, conjunctions);
+        }
+        private static bool ParseDisplayColumns(string query, out List<string> columns)
+        {
+            columns = [];
+            var match = Regex.Match(query, QueryRegex.Columns, RegexOptions.IgnoreCase);
+            if (!match.Success) return false;
+
+            var star = match.Groups[1].Value;
+
+            if (!string.IsNullOrEmpty(star))
+            {
+                columns = [star];
+            }
+            else
+            {
+                columns = match.Groups[2].Captures.Select(c => c.Value).ToList();
+                var column = match.Groups[3].Value;
+                columns.Add(column);
+            }
+
+            return true;
+        }
+        private static bool ParseWhereConditions(string conditions,
+            out List<WhereCondition> whereConditions, out List<string> conjunctions)
+        {
+            whereConditions = [];
+            conjunctions = [];
+
+            var match = Regex.Match(conditions, QueryRegex.Conditions, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                return false;
+
+            var field = match.Groups[2].Value;
+            var comparisonOperator = match.Groups[3].Value;
+            var value = match.Groups[4].Value;
+
+            conjunctions = match.Groups[5].Captures.Select(c => c.Value).ToList();
+            var fields = match.Groups[6].Captures.Select(c => c.Value).ToList();
+            var comparisonOperators = match.Groups[7].Captures.Select(c => c.Value).ToList();
+            var values = match.Groups[8].Captures.Select(c => c.Value).ToList();
+
+            if (!string.IsNullOrEmpty(field)) fields.Insert(0, field);
+            if (!string.IsNullOrEmpty(comparisonOperator)) comparisonOperators.Insert(0, comparisonOperator);
+            if (!string.IsNullOrEmpty(value)) values.Insert(0, value);
+
+            values = values.Select(value => value.Trim('\'')).ToList();
+
+            for (int i = 0; i < fields.Count; i++)
+                whereConditions.Add(new(fields[i], comparisonOperators[i], values[i]));
+
+            return true;
+        }
+        private static bool ParseAssignments(string query, out List<Assignment> assignments)
+        {
+            assignments = [];
+            var match = Regex.Match(query, QueryRegex.Assignments, RegexOptions.IgnoreCase);
+            if (!match.Success) return false;
+
+            var field = match.Groups[2].Value;
+            var value = match.Groups[3].Value;
+            var fields = match.Groups[4].Captures.Select(c => c.Value).ToList();
+            var values = match.Groups[5].Captures.Select(c => c.Value).ToList();
+            if (!string.IsNullOrEmpty(field)) fields.Insert(0, field);
+            if (!string.IsNullOrEmpty(value)) values.Insert(0, value);
+
+            values = values.Select(value => value.Trim('\'')).ToList();
+
+            for (int i = 0; i < fields.Count; i++)
+                assignments.Add(new Assignment(fields[i], values[i]));
+            return true;
+        }
+    }
+
+}
---
+++ ProjOb_24L_01180781/Database/SQL/QueryRegex.cs
@@ -0,0 +1,33 @@
+﻿using ProjOb_24L_01180781.ConsoleManagement.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOb_24L_01180781.Database.SQL
+{
+    public static class QueryRegex
+    {
+        public static readonly string NumberValue = @"-?\d+(?:\.\d+)?";
+        public static readonly string StringValue = @"'[^']*'";
+        public static readonly string Operators = @">|<|>=|<=|=|!=";
+        public static readonly string Conjunctions = @"and|or";
+        public static readonly string Name = @"[a-z]+";
+        public static readonly string NameWithDot = @$"[a-z]+\.?[a-z]*";
+
+        public static readonly string DeleteBase = $@"^({Delete.ConsoleText})\s+({Name})\s*(where)?\s*(?(3)(.*))$";
+        public static readonly string Condition = $@"({NameWithDot})\s*({Operators})\s*({NumberValue}|{StringValue})";
+        public static readonly string Conditions = $@"^({Condition})(?(1)\s*({Conjunctions})\s*{Condition}\s*)*$";
+
+        public static readonly string DisplayBase = $@"^({Display.ConsoleText})\s+(.*)\s+(from)\s+({Name})\s*(where)?\s*(?(5)(.*))$";
+        public static readonly string Columns = $@"^(?:(\*)|(?:({NameWithDot})\s*,\s*)*({NameWithDot})\s*)$";
+
+        public static readonly string AddBase = $@"^({Add.ConsoleText})\s+({Name})\s+(new)\s*\((.*)\)\s*";
+
+        public static readonly string Assignment = $@"({NameWithDot})\s*=\s*({NumberValue}|{StringValue})";
+        public static readonly string Assignments = $@"^({Assignment})\s*(?(1)(?:\s*,\s*{Assignment})*)\s*$";
+
+        public static readonly string UpdateBase = $@"^({Update.ConsoleText})\s+({Name})\s+(set)\s*\((.*)\)\s*(where)?\s*(?(5)(.*))$";
+    }
+}
